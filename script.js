9400088df47664f1c392d109329128cdde9731d4
grisha7777 <kurdjievgrigory777@gmail.com>
2021-11-05 20:42:19 +0300

    svg

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const onlineLoansBlockNumberLeft = document.querySelector('.online-loans__block__number__left');
 const onlineLoansBlockNumberRight = document.querySelector('.online-loans__block__number__right');
 const rangeSlider = document.querySelector('.range-slider input');
+const rangeSliderDate = document.querySelector('.range-slider__date input');
+const onlineLoansBlockPrice = document.querySelector('.online-loans__block__price');
+const onlineLoansBlockDatePrice = document.querySelector('.online-loans__block__date__price');
 
 
 rangeSlider.addEventListener("input", ()=>{
     let value = rangeSlider.value;
-    onlineLoansBlockNumberLeft.textContent = value;
-    onlineLoansBlockNumberRight.textContent = value;
+    onlineLoansBlockPrice.textContent = value;
+} );
+
+
+
+rangeSliderDate.addEventListener("input", ()=>{
+    let value = rangeSliderDate.value;
+    onlineLoansBlockDatePrice.textContent = value;
 } );
