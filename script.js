00482fa079621ecd2b2ab913f87e7001046aaba5
grisha7777 <kurdjievgrigory777@gmail.com>
2021-11-07 12:16:02 +0300

    adaptive-headera

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const rangeSlider = document.querySelector('.range-slider input');
 const rangeSliderDate = document.querySelector('.range-slider__date input');
 const onlineLoansBlockPrice = document.querySelector('.online-loans__block__price');
 const onlineLoansBlockDatePrice = document.querySelector('.online-loans__block__date__price');
+const burger = document.querySelector('.burger');
+const menuList = document.querySelector('.menu-list');
+const headerTopButton = document.querySelector('.header-top__button');
+
 
 
 rangeSlider.addEventListener("input", ()=>{
@@ -17,3 +21,13 @@ rangeSliderDate.addEventListener("input", ()=>{
     let value = rangeSliderDate.value;
     onlineLoansBlockDatePrice.textContent = value;
 } );
+
+
+
+burger.addEventListener('click', () =>{
+
+    menuList.classList.toggle('menu-shown');
+    headerTopButton.classList.toggle('header-top__button');
+    burger.classList.toggle('burger-rotated');
+
+})
