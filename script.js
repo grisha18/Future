a8206dd9827b4658284fd83857049c024a9b6066
grisha7777 <kurdjievgrigory777@gmail.com>
2021-11-08 16:09:38 +0300

    range-slider

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const onlineLoansBlockNumberLeft = document.querySelector('.online-loans__block__number__left');
 const onlineLoansBlockNumberRight = document.querySelector('.online-loans__block__number__right');
 const rangeSlider = document.querySelector('.range-slider input');
+const rangeSliderCircle = document.querySelector('.range-slider__circle');
+const rangeSliderCircleDate = document.querySelector('.range-slider__circle__date');
+
+
 const rangeSliderDate = document.querySelector('.range-slider__date input');
 const onlineLoansBlockPrice = document.querySelector('.online-loans__block__price');
 const onlineLoansBlockDatePrice = document.querySelector('.online-loans__block__date__price');
@@ -9,15 +13,15 @@ const menuList = document.querySelector('.menu-list');
 
 
 
-rangeSlider.addEventListener("input", ()=>{
-    let value = rangeSlider.value;
+rangeSliderCircle.addEventListener("input", ()=>{
+    let value = rangeSliderCircle.value;
     onlineLoansBlockPrice.textContent = value;
 } );
 
 
 
-rangeSliderDate.addEventListener("input", ()=>{
-    let value = rangeSliderDate.value;
+rangeSliderCircleDate.addEventListener("input", ()=>{
+    let value = rangeSliderCircleDate.value;
     onlineLoansBlockDatePrice.textContent = value;
 } );
 
